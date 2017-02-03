1511e3e6b742d2ec996ed9f144056735cd49b224
tomxue <tomxue@outlook.com>
2017-02-04 00:20:12 +0800

    update script

--- 0_git_push_both.sh
+++ 0_git_push_both.sh
@@ -4,4 +4,5 @@ git push origin --mirror
 git config remote.origin.url ssh://tomxue@45.78.43.18:29418/Learning/C/find_the_caller_function.git
 git push origin --mirror
 
+echo ""
 git config -l | grep url
